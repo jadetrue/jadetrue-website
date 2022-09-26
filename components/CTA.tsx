78d6ff26a3596c39b518fdbfd58750d89cd22459
Jade True <jadetruexo@gmail.com>
2022-09-26 08:38:32 +0100

    Fixed issue in CTA

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -11,11 +11,7 @@ const CTA: React.FC<Props> = ({ advice }) => {
     return (
         <div className="flex flex-col items-center justify-center gap-8 p-10 full-bleed bg-primary h-fit text-secondary">
             <div className="flex flex-col w-full max-w-5xl">
-                <Icon
-                    name="quote-left"
-                    prefix="fas"
-                    styles="text-6xl flex self-start"
-                />
+                <Icon icon="quote-left" styles="text-6xl flex self-start" />
                 <div className="flex max-w-2xl my-16 place-self-center">
                     <Body
                         color="primary-secondary"
@@ -25,11 +21,7 @@ const CTA: React.FC<Props> = ({ advice }) => {
                         {advice.advice}
                     </Body>
                 </div>
-                <Icon
-                    name="quote-right"
-                    prefix="fas"
-                    styles="text-6xl flex self-end"
-                />
+                <Icon icon="quote-right" styles="text-6xl flex self-end" />
             </div>
         </div>
     );
