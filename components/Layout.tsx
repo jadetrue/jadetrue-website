232339cddf554566ec833790c601cb7925de5451
Jade True <jadetruexo@gmail.com>
2022-09-26 09:12:45 +0100

    Updated preview image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -64,9 +64,7 @@ export default function Layout({
                 />
                 <meta
                     property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    content="https://www.jadetrue.co.uk/preview.png"
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
@@ -84,7 +82,6 @@ export default function Layout({
                     crossOrigin="anonymous"
                 ></script>
                 <script
-                  
                     src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
                 />
                 <script>
