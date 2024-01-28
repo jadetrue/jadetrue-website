8bced30d71acb4c422d0cd97ca47cd1ff35d5075
Jade True <jadetruexo@gmail.com>
2024-01-28 20:15:25 +0000

    Add skills subtitle and update advice

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -4,27 +4,34 @@ import { Body } from "./Typography";
 import { Advice } from "../models/Advice";
 
 export interface Props {
-    advice: Advice;
+  advice: Advice;
 }
 
 const CTA: React.FC<Props> = ({ advice }) => {
-    return (
-        <div className="flex flex-col items-center justify-center gap-8 p-10 full-bleed bg-primary h-fit text-secondary">
-            <div className="flex flex-col w-full max-w-5xl">
-                <Icon icon="quote-left" styles="text-6xl flex self-start" />
-                <div className="flex max-w-2xl my-16 place-self-center">
-                    <Body
-                        color="primary-secondary"
-                        styles="p-2 flex justify-center"
-                        size="extra-large"
-                    >
-                        {advice.advice}
-                    </Body>
-                </div>
-                <Icon icon="quote-right" styles="text-6xl flex self-end" />
-            </div>
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center gap-8 p-10 full-bleed bg-primary h-fit text-secondary">
+        <div className="flex flex-col w-full max-w-5xl">
+          <Icon icon="quote-left" styles="text-6xl flex self-start" />
+          <div className="flex max-w-2xl my-16 place-self-center">
+            <Body
+              color="primary-secondary"
+              styles="p-2 flex justify-center"
+              size="extra-large"
+            >
+              {advice.advice}
+            </Body>
+          </div>
+          <Icon icon="quote-right" styles="text-6xl flex self-end" />
         </div>
-    );
+      </div>
+      <div className="w-full flex justify-start md:justify-end p-2">
+        <Body size="extra-small">
+          *Quotes are randomly generated upon refreshing my webpage.
+        </Body>
+      </div>
+    </>
+  );
 };
 
 export default CTA;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,106 +1,73 @@
 import React from "react";
 import { Body, Header } from "./Typography";
-import { XyzTransition, XyzTransitionGroup } from "@animxyz/react";
+import { XyzTransitionGroup } from "@animxyz/react";
 
 const Skills = () => {
-    return (
-        <div id="skills">
-            <Header styles="text-center w-full pt-8 justify-center">
-                What I use
-            </Header>
-            <div className="grid items-center justify-center w-full grid-cols-2 gap-16 m-auto my-24 overflow-hidden md:gap-8 md:grid-cols-3">
-                <SkillItem key="1">
-                    <img
-                        src="/images/skills-html.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>HTML 5</Body>
-                </SkillItem>
-                <SkillItem key="2">
-                    <img
-                        src="/images/skills-css3.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>CSS 3</Body>
-                </SkillItem>
-                <SkillItem key="3">
-                    <img
-                        src="/images/skills-javascript.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>JavaScript</Body>
-                </SkillItem>
-                <SkillItem key="4">
-                    <img
-                        src="/images/skills-react.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>React</Body>
-                </SkillItem>
-                <SkillItem key="5">
-                    <img
-                        src="/images/skills-typescript.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>TypeScript</Body>
-                </SkillItem>
-                <SkillItem key="6">
-                    <img
-                        src="/images/skills-nodejs.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>NodeJS</Body>
-                </SkillItem>
-                <SkillItem key="7">
-                    <img
-                        src="/images/skills-tailwind.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>Tailwind CSS</Body>
-                </SkillItem>
-                <SkillItem key="8">
-                    <img
-                        src="/images/skills-git.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>Git</Body>
-                </SkillItem>
-                <SkillItem key="9">
-                    <img
-                        src="/images/skills-npm.png"
-                        height={100}
-                        width={100}
-                    />
-                    <Body>npm</Body>
-                </SkillItem>
-            </div>
-        </div>
-    );
+  return (
+    <div id="skills">
+      <Header styles="text-center w-full pt-8 justify-center">
+        What I use
+      </Header>
+      <div className="w-full my-24 md:text-center flex flex-col place-items-center">
+        <Body>But not limited to.</Body>
+      </div>
+      <div className="grid items-center justify-center w-full grid-cols-2 gap-16 m-auto my-24 overflow-hidden md:gap-8 md:grid-cols-3">
+        <SkillItem key="1">
+          <img src="/images/skills-html.png" height={100} width={100} />
+          <Body>HTML 5</Body>
+        </SkillItem>
+        <SkillItem key="2">
+          <img src="/images/skills-css3.png" height={100} width={100} />
+          <Body>CSS 3</Body>
+        </SkillItem>
+        <SkillItem key="3">
+          <img src="/images/skills-javascript.png" height={100} width={100} />
+          <Body>JavaScript</Body>
+        </SkillItem>
+        <SkillItem key="4">
+          <img src="/images/skills-react.png" height={100} width={100} />
+          <Body>React</Body>
+        </SkillItem>
+        <SkillItem key="5">
+          <img src="/images/skills-typescript.png" height={100} width={100} />
+          <Body>TypeScript</Body>
+        </SkillItem>
+        <SkillItem key="6">
+          <img src="/images/skills-nodejs.png" height={100} width={100} />
+          <Body>NodeJS</Body>
+        </SkillItem>
+        <SkillItem key="7">
+          <img src="/images/skills-tailwind.png" height={100} width={100} />
+          <Body>Tailwind CSS</Body>
+        </SkillItem>
+        <SkillItem key="8">
+          <img src="/images/skills-git.png" height={100} width={100} />
+          <Body>Git</Body>
+        </SkillItem>
+        <SkillItem key="9">
+          <img src="/images/skills-npm.png" height={100} width={100} />
+          <Body>npm</Body>
+        </SkillItem>
+      </div>
+    </div>
+  );
 };
 
 interface SkillItemProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const SkillItem: React.FC<SkillItemProps> = ({ children }) => {
-    return (
-        <XyzTransitionGroup
-            appearVisible
-            xyz="fade down-5 rotate-right-50% stagger duration-10"
-        >
-            <div className="flex flex-col items-center justify-center gap-4 m-auto text-center">
-                {children}
-            </div>
-        </XyzTransitionGroup>
-    );
+  return (
+    <XyzTransitionGroup
+      appearVisible
+      xyz="fade down-5 rotate-right-50% stagger duration-10"
+    >
+      <div className="flex flex-col items-center justify-center gap-4 m-auto text-center">
+        {children}
+      </div>
+    </XyzTransitionGroup>
+  );
 };
 
 export default Skills;
